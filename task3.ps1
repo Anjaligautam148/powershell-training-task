6d23132336e75729fc4f22b37bebb08103e4d66f
Anjaligautam148 <101336772+Anjaligautam148@users.noreply.github.com>
2022-04-02 23:44:41 +0530

    Create task3.ps1

---
+++ task3.ps1
@@ -0,0 +1,10 @@
+ Check that a person is eligible for voting or not. 
+ $Age = 23
+ if($Age -ge 18)
+ {
+ echo "The Person is Elible for Vote" $Age
+ }
+ else
+ {
+ echo "The Person is Not Eligible for Vote" $Age
+ } 
