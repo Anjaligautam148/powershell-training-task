b996f756c6a5c54768d14bb1fb632275aebb50f5
Anjaligautam148 <101336772+Anjaligautam148@users.noreply.github.com>
2022-04-02 23:53:01 +0530

    Create task4.ps1

---
+++ task4.ps1
@@ -0,0 +1,11 @@
+ Check the strings in variable 'a' and 'b' are same.
+ $a = "I unterstand this task"
+ $b = "I understand this task"
+ if($a -eq $b)
+ {
+ echo "  string is equal"
+ }
+ else
+ {
+ echo " string is not equal"
+ } 
