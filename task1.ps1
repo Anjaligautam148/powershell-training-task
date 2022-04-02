db15f589b18d6271de1723ee8d6643d21c3aa15d
Anjaligautam148 <101336772+Anjaligautam148@users.noreply.github.com>
2022-04-02 23:32:53 +0530

    Create task1.ps1

---
+++ task1.ps1
@@ -0,0 +1,6 @@
+Check that the value of the variable is either a 10, 30 or 1000. If none of these value matches, then the default statement is executed. 
+switch(3){ 
+1 {"10"} 
+2 {"30"} 
+3 {"1000"} 
+default {" Number is not matches"} }
