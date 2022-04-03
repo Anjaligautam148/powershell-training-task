dfec7ed7cd7433f72de7f6107592b991688dfc9c
Anjaligautam148 <101336772+Anjaligautam148@users.noreply.github.com>
2022-04-03 19:40:51 +0530

    Create evenoddno.ps1

---
+++ evenoddno.ps1
@@ -0,0 +1,10 @@
+Write script to check number is even or odd.
+$a = Read-Host -Prompt "Enter Number"
+ if($a%2 -eq 0)
+ {
+ Write-Host "  Number is Even"
+ }
+ else
+ {
+ Write-Host " Number is Odd"
+ } 
