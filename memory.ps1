75dac7437063bc27d3fa30c920559a313f8111f6
Anjaligautam148 <101336772+Anjaligautam148@users.noreply.github.com>
2022-04-03 20:39:56 +0530

    Create memory.ps1

---
+++ memory.ps1
@@ -0,0 +1,4 @@
+ Calculate the average Memory Utilization.
+ $sysname = Get-WmiObject -Class WIN32_OperatingSystem 
+ $Memory = ((($sysname.TotalVisibleMemorySize - $sysname.FreePhysicalMemory)*100)/ $sysname.TotalVisibleMemorySize) 
+ Write-Host "Memory usage in Percentage:" $Memory -ForegroundColor blue 
