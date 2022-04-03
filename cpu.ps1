9ec367ff4fbffc6a44557aedcd0bc5f18a20a88f
Anjaligautam148 <101336772+Anjaligautam148@users.noreply.github.com>
2022-04-03 20:43:15 +0530

    Create cpu.ps1

---
+++ cpu.ps1
@@ -0,0 +1,4 @@
+Calculate the average CPU Utilization.
+$cpuutilization = (GET-COUNTER -Counter "\Processor(_Total)\% Processor Time" |select -ExpandProperty countersamples | select -
+ ExpandProperty cookedvalue | Measure-Object -Average).average
+ Write-Host "cpu usage in percentage: " $cpuutilization -ForegroundColor cyan 
